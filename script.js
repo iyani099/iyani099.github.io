cfa4be20d61aa462d14fef3d03a6c3ec66ec44be
Williamsusman <33702238+Williamsusman@users.noreply.github.com>
2019-05-25 11:18:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const math = require("math.js")
 
+var questions = ["#1", "#2", "#3", "#4", "#5"]
+var randomQuestion = math.floor((math.random()* questions.length));          
+                 
+  
+               
