6ff0100b56011b12164e48020e3e6868ebf1fedc
Williamsusman <33702238+Williamsusman@users.noreply.github.com>
2019-05-27 21:43:18 -0400

    Update script.js

--- Units/script.js
+++ Units/script.js
@@ -11,11 +11,11 @@ function populate(){
 }
 
 var questions = [
-  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
-  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
-  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
-  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
-  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question1", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question2", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question3", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question4", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question5", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
 ]
 
 var quiz = new quiz(questions);
