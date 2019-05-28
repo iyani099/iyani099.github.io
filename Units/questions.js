8358a37b09589e231355debad9e619d7be79deea
Williamsusman <33702238+Williamsusman@users.noreply.github.com>
2019-05-27 21:20:05 -0400

    Update questions.js

--- Units/questions.js
+++ Units/questions.js
@@ -1,1 +1,11 @@
-
+function Questions(text, choices, answer){
+  this.text = text;
+  this.choices = choices
+  this.answer = answer;
+  
+  
+  
+}
+Questions.prototype.correctAnswer = function(choice){
+  return choice === this.answer;
+}
