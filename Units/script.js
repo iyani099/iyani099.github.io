e666e6d80d5e0c2f2cdff78b6adbd1475e6fe2ce
Williamsusman <33702238+Williamsusman@users.noreply.github.com>
2019-05-27 21:40:27 -0400

    Update script.js

--- Units/script.js
+++ Units/script.js
@@ -1,1 +1,23 @@
+function populate(){
+  if(quiz.isEnded()){
+    //showScore();
+  }
+  else{
+    //show Question
+    var element = document.getElementById("question");
+    element.innerHTML = quiz.getQuestionIndex().text;
+    
+  }
+}
 
+var questions = [
+  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+  new Questions("question", ["choice1", "choice2", "choice3", "choice4"], "correctAnswer")
+]
+
+var quiz = new quiz(questions);
+
+populate();
