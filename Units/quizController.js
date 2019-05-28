484d134546dd420b1b2b90a00c22f45f477ae516
Williamsusman <33702238+Williamsusman@users.noreply.github.com>
2019-05-27 21:30:52 -0400

    Update quizController.js

--- Units/quizController.js
+++ Units/quizController.js
@@ -1,1 +1,19 @@
+function quiz(questions){
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
 
+quiz.prototype.getQuestionIndex = function(){
+  return this.questions[this.questionIndex]
+}
+quiz.prototype.isEnded = function(){
+  return this.questions.length == this.questionIndex
+}
+
+quiz.prototype.guess = function(answer){
+  this.questionIndex++;
+  if(this.getQuestionIndex().correctAnswer(answer)){
+    this.score++;
+  }
+}
